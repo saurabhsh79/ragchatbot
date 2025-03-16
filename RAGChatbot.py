d7146e2a23388b7092ee263c3dd59f3044a5b104
saurabhsh79 <saurabhsh79@gmail.com>
2025-03-16 17:27:09 +0530

    Create RAGChatbot.py

---
+++ RAGChatbot.py
@@ -0,0 +1,5 @@
+st.title("Financial RAG Chatbot")
+user_query = st.text_input("Ask a financial question:")
+if user_query:
+    response = retrieve_answer(user_query)
+    st.write("Answer:", response)
